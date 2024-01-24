bbbae0aeee438cd3060720180e16f5069c076040
Filip Szczypiński <47000594+fiszczyp@users.noreply.github.com>
2024-01-24 15:06:58 +0000

    Update __init__.py
    Signed-off-by: Filip Szczypiński <47000594+fiszczyp@users.noreply.github.com>

--- src/fourier_nmr_driver/__init__.py
+++ src/fourier_nmr_driver/__init__.py
@@ -3,9 +3,9 @@ from fourier_nmr_driver.driver import Fourier80, NMRExperiment, TopSpinWarning
 
 __version__ = "0.2.0"
 __all__ = (
-    NMRExperiment,
-    Fourier80,
-    TopSpinWarning,
-    PARAMS,
-    SOLVENTS,
+    "NMRExperiment",
+    "Fourier80",
+    "TopSpinWarning",
+    "PARAMS",
+    "SOLVENTS",
 )
